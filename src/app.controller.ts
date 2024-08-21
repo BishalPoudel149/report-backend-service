33b11297c9adfba61596961c6182e0cc42aa65d9
Bishal Poudel <bishal.poudel@sap.com>
2024-08-21 23:16:50 +0530

    update default endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,8 +4,11 @@ import { time } from 'console';
 import { TestDto } from './dto/test.dto';
 import { DatabaseService } from './services/database.service';
 
-@Controller('trend')
+@Controller()
 export class AppController {
+  getHello(): any {
+    throw new Error('Method not implemented.');
+  }
   constructor(private readonly appService: AppService,
     private readonly databaseService: DatabaseService,
   ) {}
@@ -13,18 +16,25 @@ export class AppController {
   days=1;
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getEndpoints(): Record<string, string> {
+    // List all available endpoints and their descriptions
+    return {
+      '/': 'Returns a list of all available endpoints',
+      '/user/login' : 'POST  to Login user ',
+
+      '/customer/:customeremail':'Get ,Fetch all the products information for particular Customer'
+      
+    };
   }
 
-  @Get('/:productCategory')
+  @Get('trend/:productCategory')
   getTrend(@Param('productCategory') productCategory?:string,@Param('timerange') timeRange?:string){
     if(!productCategory)
     productCategory='Beverages';
      return  this.appService.getTrend(productCategory);
   }
 
-  @Post('/createtest')
+  @Post('trend/createtest')
   createTest(@Body() testDto:TestDto){
     return this.databaseService.createTest(testDto);
   }
